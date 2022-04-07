3c200570b75381775c21c82340b44746570eb0f8
CaseyNorthe <caseynorth182@gmail.comm>
2022-04-07 20:08:28 +0300

    filter on class

--- src/components/card-filter/CardFilter.js
+++ src/components/card-filter/CardFilter.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { Component } from "react";
+import Loader from "../loader/Loader";
 
 const FilterArr = {
     cats: [
@@ -46,6 +47,7 @@ const FilterArr = {
             descr: '21 и 22 мая 2022 на нижней Набережной (акватория г. Ингул) состоится фестиваль на воде «Николаев River FEST».'
         },
         {
+            cat: 'Фестивалі',
             image: 'https://tic.sladko.digital/wp-content/uploads/2021/12/26172621_1956733717676582_3484499359655914309_o5a4ca91e5f234-1536x590.jpg',
             title: '2222Празднование Нового 2022 года на Соборной площади22222',
             date: '31.12.2021 – 31.12.2021',
@@ -71,89 +73,146 @@ const FilterArr = {
     ]
 }
 
-const catsTitleDescktop = () => {
-    return FilterArr.cats.map(item => {
-        let tabClass = '';
-        if (item.active) {
-            tabClass = 'active';
-        }
-        return (
-            <li className="tabs-nav__name tabs-nav "><a className={tabClass} href="#tab-1">{item.name}</a></li>
-        )
-    })
-}
 
-const CardFilter = () => {
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-        return setItems(FilterArr.card)
-    })
-    console.log(items);
-    const cardsView = items.map((item, i) => {
-        if (i < 4) {
+
+
+class CardFilter extends Component {
+    state = {
+        data: [],
+        term: '',
+        loading: true,
+    }
+    componentDidMount() {
+        let newItems = FilterArr.card.map((item, i) => {
+            return item;
+        })
+        this.setState(({ data }) => {
+            return {
+                data: newItems,
+                loading: false,
+            }
+
+        })
+    }
+    onCatChange = (e) => {
+        e.preventDefault();
+        let catItem = e.target.getAttribute('data-filter');
+        let newSet;
+        if (catItem === 'Фестивалі') {
+            newSet = FilterArr.card.filter((item, i) => {
+                return item.cat === 'Фестивалі';
+            })
+        } else if (catItem === 'Концерти') {
+            newSet = FilterArr.card.filter((item, i) => {
+                return item.cat === 'Концерти';
+            })
+        } else if (catItem === 'Виставки') {
+            newSet = FilterArr.card.filter((item, i) => {
+                return item.cat === 'Виставки';
+            })
+        } else {
+            newSet = FilterArr.card.filter((item, i) => {
+                return item;
+            })
+        }
+        this.setState({
+            data: newSet
+        })
+    }
+
+    catsTitle = () => {
+        const componentsCat = FilterArr.cats.map(item => {
             return (
-                <article className="col-lg-3 col-md-6 col-sm-6 p-2 col-12 tabs-item__card event-card">
-                    <div className="event-card__inner">
-                        <picture className="event-card__picture">
-
-                            <img className="event-card__img" src={item.image} alt="#" />
-                        </picture>
-                        <div className="event-card__content">
-                            <div className="event-card__header">
-                                <span className="event-card__date">
-                                    {item.date}
-                                </span>
-                                <img src={item.icon} alt="" />
-                            </div>
-                            <a href="#" className="event-card__title">
-                                {item.title}
-                            </a>
-                            <p className="event-card__text">
-                                {item.descr}
-                            </p>
-                            <span className="event-card__location">
-                                с.
-                                Трихати
-                            </span>
-                        </div>
-                    </div>
-                </article>
+                <li className="tabs-nav__name tabs-nav ">
+                    <a
+                        onClick={this.onCatChange}
+                        data-filter={item.name}
+                        href="#">
+                        {item.name}
+                    </a>
+                </li>
             )
-        }
-    })
-    return (
-        <>
-            <div className="event-blog__slider-btn">
-                <div className="swiper-button-prev--event swiper-button-prev" />
-                <div className="swiper-button-next--event swiper-button-next" />
-            </div>
-            <div className="event-blog__wrapper">
-                <div id="tabs">
-                    {/* Кнопки */}
-                    <ul className="tabs-nav  tabs-nav--descktop">
-                        {catsTitleDescktop()}
-                    </ul>
-                    <div className="swiper tabs-nav--mobile">
-                        <ul className="swiper-wrapper">
-                            {catsTitleDescktop()}
-                        </ul>
-                    </div>
-                    {/* Контент */}
-                    <div className="tabs-items">
-                        <div className="tabs-item " id="tab-1">
-                            <div className="row d-flex event-card__wrapper">
-                                {cardsView}
+        })
+        return componentsCat;
+    }
+    cardsView = () => {
 
-                            </div>
-                            <div className="tabs-items__btn"><a className="tabs-items__btn-link" href="#">Дивитись всі</a>
+        return this.state.data.map((item, i) => {
+            console.log(item);
+            if (i < 4) {
+                return (
+
+                    <article className="col-lg-3 col-md-6 col-sm-6 p-2 col-12 tabs-item__card event-card">
+                        <div className="event-card__inner">
+                            <picture className="event-card__picture">
+
+                                <img className="event-card__img" src={item.image} alt="#" />
+                            </picture>
+                            <div className="event-card__content">
+                                <div className="event-card__header">
+                                    <span className="event-card__date">
+                                        {item.date}
+                                    </span>
+                                    <img src={item.icon} alt="" />
+                                </div>
+                                <a href="#" className="event-card__title">
+                                    {item.title}
+                                </a>
+                                <p className="event-card__text">
+                                    {item.descr}
+                                </p>
+                                <span className="event-card__location">
+                                    с.
+                                    Трихати
+                                </span>
                             </div>
                         </div>
+                    </article>
+                )
+            }
+        })
+    }
 
+
+    render() {
+        const { loading } = this.state;
+        const viewCards = loading ? <Loader /> : this.cardsView();
+        return (
+            <>
+                <div className="event-blog__slider-btn">
+                    <div className="swiper-button-prev--event swiper-button-prev" />
+                    <div className="swiper-button-next--event swiper-button-next" />
+                </div>
+                <div className="event-blog__wrapper">
+                    <div id="tabs">
+                        {/* Кнопки */}
+                        <ul className="tabs-nav  tabs-nav--descktop">
+                            {this.catsTitle()}
+
+                        </ul>
+                        <div className="swiper tabs-nav--mobile">
+                            <ul className="swiper-wrapper">
+
+                            </ul>
+                        </div>
+                        {/* Контент */}
+                        <div className="tabs-items">
+                            <div className="tabs-item " id="tab-1">
+                                <div className="row d-flex event-card__wrapper">
+                                    {viewCards}
+
+                                </div>
+                                <div className="tabs-items__btn"><a className="tabs-items__btn-link" href="#">Дивитись всі</a>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
+
 }
 
 export default CardFilter;
